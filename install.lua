54ad15c99e0b8ca64c76aed733b838fc50f0c9f0
Stephen <sleitnick@gmail.com>
2016-05-29 11:27:19 -0400

    Installation script update
    Now gives a visual progress display instead of printing to the output
    window.

--- install.lua
+++ install.lua
@@ -5,10 +5,57 @@
 -- Crazyman32
 -- December 25, 2015
 
+-- Updated May 29, 2016
+	-- Added GUI progress screen
+
 
 local http = game:GetService("HttpService")
 local originallyEnabled = http.HttpEnabled
 http.HttpEnabled = true
+
+
+local loadingGui = Instance.new("ScreenGui")
+	loadingGui.Name = "TeamCrazyGameFrameworkLoadingGui"
+	loadingGui.Archivable = false
+	local frameGui = Instance.new("Frame", loadingGui)
+	frameGui.Position = UDim2.new(0.5, -125, 0, 20)
+	frameGui.Size = UDim2.new(0, 250, 0, 50)
+	frameGui.Style = Enum.FrameStyle.DropShadow
+	local labelGui = Instance.new("TextLabel", frameGui)
+	labelGui.Name = "Label"
+	labelGui.BackgroundTransparency = 1
+	labelGui.Position = UDim2.new(0, 0, 0, -4)
+	labelGui.Size = UDim2.new(1, -40, 0.5, 0)
+	labelGui.Font = Enum.Font.SourceSans
+	labelGui.FontSize = Enum.FontSize.Size14
+	labelGui.TextColor3 = Color3.new(1, 1, 1)
+	labelGui.TextXAlignment = Enum.TextXAlignment.Left
+	labelGui.ClipsDescendants = true
+	labelGui.Text = "Initializing..."
+	local percentGui = Instance.new("TextLabel", frameGui)
+	percentGui.Name = "Percent"
+	percentGui.BackgroundTransparency = 1
+	percentGui.Position = UDim2.new(0, 0, 0, -4)
+	percentGui.Size = UDim2.new(1, 0, 0.5, 0)
+	percentGui.Font = Enum.Font.SourceSansBold
+	percentGui.FontSize = Enum.FontSize.Size14
+	percentGui.TextColor3 = Color3.new(1, 1, 1)
+	percentGui.TextXAlignment = Enum.TextXAlignment.Right
+	percentGui.Text = "0%"
+	local progressGui = Instance.new("Frame", frameGui)
+	progressGui.Name = "Progress"
+	progressGui.BackgroundColor3 = Color3.new(1, 1, 1)
+	progressGui.BorderSizePixel = 0
+	progressGui.Position = UDim2.new(0, 0, 0.5, 0)
+	progressGui.Size = UDim2.new(1, 0, 0.5, 0)
+	local barGui = Instance.new("Frame", progressGui)
+	barGui.Name = "Bar"
+	barGui.BackgroundColor3 = Color3.new(0, (2 / 3), 0.5)
+	barGui.BorderSizePixel = 0
+	barGui.Position = UDim2.new(0, 0, 0, 0)
+	barGui.Size = UDim2.new(0, 0, 1, 0)
+	
+loadingGui.Parent = game:GetService("CoreGui")
 
 
 local filelistUrl = "https://raw.githubusercontent.com/Sleitnick/TeamCrazyGameFramework/master/filelist.txt"
@@ -29,7 +76,8 @@ table.sort(filelist, function(a, b)
 	return (#a < #b)
 end)
 
-print(("\nDownloading %i file%s...\n"):format(#filelist, #filelist == 1 and "" or "s"))
+--print(("\nDownloading %i file%s...\n"):format(#filelist, #filelist == 1 and "" or "s"))
+labelGui.Text = ("Downloading %i file%s...\n"):format(#filelist, #filelist == 1 and "" or "s")
 
 
 
@@ -76,11 +124,19 @@ end
 local numFiles = #filelist
 for i,path in pairs(filelist) do
 	local s = CreateScript(path)
-	print(("[%i / %i] %s"):format(i, numFiles, s:GetFullName()))
+	local completed = (i / numFiles)
+	--print(("[%i / %i] %s"):format(i, numFiles, s:GetFullName()))
+	labelGui.Text = ("[%i / %i] %s..."):format(i, numFiles, s.Name)
+	percentGui.Text = ("%.i%%"):format(completed * 100)
+	barGui.Size = UDim2.new(completed, 0, 1, 0)
 	DownloadSource(s, path)
 end
 
 
 http.HttpEnabled = originallyEnabled
 
-print("Team Crazy Game Framework downloaded")
+--print("Team Crazy Game Framework downloaded")
+labelGui.Text = "Download & installation completed."
+percentGui.Text = ""
+wait(3)
+loadingGui:Destroy()
